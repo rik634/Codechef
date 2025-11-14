1523ae491e8d72cc4cad31a6c46898e9d217ff4b
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-11-14 10:44:34 +0530

    Create Fuel check.cpp

---
+++ Fuel check.cpp
@@ -0,0 +1,22 @@
+// contest 211
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin >> X >> Y;
+	int d = X*Y;
+	if(d>=100)
+	{
+	    cout << "Yes";
+	}
+	else
+	{
+	    cout << "No";
+	}
+
+}
+
+// Time complexity O(1)
+// Space complexity: O(1)
